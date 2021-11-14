9c032c37d46fdacc41aea1af8d5293d30cb747d0
llcruz1 <lucaslimacruz1@gmail.com>
2021-11-14 20:50:07 -0300

    First commit

---
+++ src/store/index.js
@@ -0,0 +1,14 @@
+import { createStore } from "vuex";
+
+export default createStore({
+  state: {
+    invoiceModal: false,
+  },
+  mutations: {
+    TOGGLE_INVOICE(state) {
+      state.invoiceModal = !state.invoiceModal;
+    },
+  },
+  actions: {},
+  modules: {},
+});
